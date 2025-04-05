715c47af3d84ff70cea7dfebf97a1e07637580d2
Martin Coxall <pseudo.meta@gmail.com>
2025-04-05 03:08:04 +0100

    concatenative borf is the way

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Core library for the Borf language
-
-pub mod evaluator;
-pub mod integration;
-pub mod parser;
-pub mod runtime;
-pub mod semantics;
--- src/semantics.rs
+++
@@ -1,1 +0,0 @@
-// This module will contain semantics-related functionality in the future
